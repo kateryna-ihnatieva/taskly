049acb0b85cc8cce393c2c34cf62bdd88ae4fad0
Kateryna Ihnatieva <ihnatieva.kateryna@gmail.com>
2025-07-07 15:21:40 +0300

    Added ordering by priority

--- tasks/views.py
+++ tasks/views.py
@@ -15,4 +15,4 @@ class TaskViewSet(viewsets.ModelViewSet):
     ]
     filterset_fields = ["is_completed", "priority"]
     search_fields = ["title", "description"]
-    ordering_fields = ["created_at", "updated_at", "is_completed"]
+    ordering_fields = ["created_at", "updated_at", "is_completed", "priority_order"]
